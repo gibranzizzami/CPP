a43ce5fb67821f82677139c8cbcd3a295c0a7869
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-17 13:58:13 +0700

    update

--- learning.cpp
+++ learning.cpp
@@ -1,19 +1,16 @@
-// make folder
+// made folder
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <iostream>
-
-int main() {
-    mkdir("FolderBaru", 0777);
-    std::cout << "Folder berhasil dibuat!" << std::endl;
-    return 0;
-}
-
-// make file
 #include <iostream>
 #include <fstream>
 
 int main() {
-    std::ofstream file("contoh.txt");
+    // made folder
+    mkdir("FolderBaru", 0777);
+    std::cout << "Folder berhasil dibuat!" << std::endl;
+}
+
+    // made file
+     std::ofstream file("contoh.txt");
     return 0;
 }
