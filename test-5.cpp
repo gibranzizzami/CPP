ec5780a519e5abcd17716b5f2982e490592470bf
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-19 16:07:17 +0700

    update

--- test-5.cpp
+++ test-5.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string namaFile;
+    cout << "Masukkan nama file yang ingin diedit: ";
+    getline(cin, namaFile);
+
+    ifstream file(namaFile);
+    if (!file) {
+        cout << "File tidak ditemukan!\n";
+        return 1;
+    }
+
+    string isi;
+    getline(file, isi);
+    file.close();
+
+    // Cek isi file
+    if (isi == "hello world") {
+        isi += "!";
+        
+        ofstream out(namaFile);
+        out << isi;
+        out.close();
+
+        cout << "Tanda seru berhasil ditambahkan!\n";
+    } else {
+        cout << "Isi file bukan 'hello world', tidak ada perubahan.\n";
+    }
+
+    return 0;
+}
