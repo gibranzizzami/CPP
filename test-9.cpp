451ae6be758a897cc9de88f1346e61b62b700ea9
gibranzizzami <gbrnzzmi24@gmail.com>
2026-02-19 21:53:55 +0700

    update

--- test-9.cpp
+++ test-9.cpp
@@ -4,27 +4,26 @@
 namespace fs = std::filesystem;
 
 int main() {
-    std::string sumber, tujuan;
+    std::string sumber, tujuan;  // folder asal dan folder tujuan
 
-    std::cout << "Masukkan path folder sumber: ";
+    std::cout << "Masukkan path folder sumber: "; // user memilih folder sumber
     std::getline(std::cin, sumber);
 
-    std::cout << "Masukkan path folder tujuan: ";
+    std::cout << "Masukkan path folder tujuan: "; // user memilih folder tujuan
     std::getline(std::cin, tujuan);
 
     try {
-        fs::copy(
+        fs::copy(  // memanggil fungsi copy dari filesystem
             sumber,
             tujuan,
-            fs::copy_options::recursive |
-            fs::copy_options::overwrite_existing
+            fs::copy_options::recursive |  // isinya ikut dicopy
+            fs::copy_options::overwrite_existing // ditimpa isinya kalo ada
         );
 
         std::cout << "Folder berhasil dicopy!\n";
     }
-    catch (fs::filesystem_error& e) {
-        std::cout << "Error: " << e.what() << "\n";
+    catch (fs::filesystem_error& e) {  // jika terjadi eror
+        std::cout << "Error: " << e.what() << "\n"; // pesan erornya
     }
-
     return 0;
 }
